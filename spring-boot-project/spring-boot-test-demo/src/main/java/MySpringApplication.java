de3fcdd50d9523b92263c0c989954ebbabd01a23
gaoxx <gaoxx@fxiaoke.com>
2019-01-16 10:43:17 +0800

    spring boot sources init

---
+++ spring-boot-project/spring-boot-test-demo/src/main/java/MySpringApplication.java
@@ -0,0 +1,17 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+
+/**
+ * @author Ant
+ * @ClassName: MySpringApplication
+ * @Description: springbootApplication
+ * @datetime 2019/1/16 10:27
+ * @Version 1.0
+ */
+@SpringBootConfiguration
+public class MySpringApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringApplication.class);
+		System.out.println("-------------------------- OJBK -----------------------");
+	}
+}
